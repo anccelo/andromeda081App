e39d71ba06f03eed37702925d07683d2833580e7
Angelo La Greca <anccelo@iol.it>
2020-04-29 16:37:40 +0200

    made a first planet inerface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+import {Planet} from './planet';
+import {PLANETS} from './mock-planets';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'andromeda081App';
+
+export class AppComponent implements OnInit {
+  title = 'Solar system planets';
+  private planets: Planet[];
+
+  ngOnInit() {
+    this.planets = PLANETS;
+  }
+
+  selectPlanet(planet: any) {
+    alert('You have chosen ' + planet.name);
+  }
 }
